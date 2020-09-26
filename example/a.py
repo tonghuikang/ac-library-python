efdd1420d9228dc771d8cb27dc83dd91679e1e44
hkmac <tonghuikang@outlook.com>
2020-09-26 20:33:15 +0800

    take deep breath

--- example/a.py
+++ example/a.py
@@ -13,41 +13,30 @@ from collections import Counter, defaultdict
 def solve(grid, n):  # fix inputs here
     console("----- solving ------")
     grid = [(x-1, y-1) for x,y in grid]
-
-    m = {}
-    for i,(x,y) in enumerate(grid):
-        m[x] = i
     
     grid = sorted(grid)
-    console(grid)
+    console(grid, n)
 
     g = defaultdict(list)
-    y_heap = []
-    heapq.heapify(y_heap)
 
     for x,y in grid:
-        if y_heap:
-            ymin,xmin = y_heap[0]
-            if ymin < y:
-                while y_heap and y_heap[0] < (y,x):
-                    ycur,xcur = heapq.heappop(y_heap)
-                    g[x].append(xcur)
-                    g[xcur].append(x)
-                heapq.heappush(y_heap, (ymin,xmin))
-        heapq.heappush(y_heap, (y,x))
-        
+        g[x].append(y)
+        g[y].append(x)
+
     console(g)
 
-    res = [0 for _ in grid]
-    visited_1 = [0 for _ in grid]
-    visited_2 = [0 for _ in grid]
+    res = [0 for _ in range(n)]
+    visited_1 = [0 for _ in range(n)]
+
+    result = 0  # number of connected components
 
     for i in range(n):
-        if res[i]:  # if visited
+        if visited_1[i]:  # if visited
             continue
         visited_1[i] = 1
         stack = [i]
         cnt = 1
+        result += 1
         while stack:
             cur = stack.pop()
             for nex in g[cur]:
@@ -56,25 +45,15 @@ def solve(grid, n):  # fix inputs here
                     visited_1[nex] = 1
                     stack.append(nex)
                 
-        # print(i,cnt)
-        visited_2[i] = 1
-        stack = [i]
-        while stack:
-            cur = stack.pop()
-            res[cur] = cnt
-            for nex in g[cur]:
-                if not visited_2[nex]:
-                    visited_2[nex] = 1
-                    stack.append(nex)
 
-    console(res)
+    # console(res)
+    console(result)
     console(visited_1)
-    console(visited_2)
-    res2 = [0 for _ in grid]
-    for i,x in m.items():
-        res2[x] = res[i]
+    # res2 = [0 for _ in grid]
+    # for i,x in m.items():
+    #     res2[x] = res[i]
 
-    return res2
+    return result - 1
 
 
 
@@ -91,13 +70,13 @@ for case_num in [1]:
     # strr = input()
 
     # read line as an integer
-    nrows = int(inp[0])
+    # nrows = int(inp[0])
     
     # read one line and parse each word as a string
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # lst = list(map(int,input().split()))
+    k, nrows = list(map(int,inp[0].split()))
 
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
@@ -106,9 +85,9 @@ for case_num in [1]:
     for n in range(nrows):
         grid.append(list(map(int,inp[n+1].split())))
 
-    res = solve(grid, nrows)  # please change
+    res = solve(grid, k)  # please change
     
-    res = "\n".join(str(x) for x in res)
+    # res = "\n".join(str(x) for x in res)
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
--- example/d.py
+++ example/d.py
@@ -3,47 +3,22 @@ import heapq, functools, collections
 import math, random
 from collections import Counter, defaultdict
 
-from atcoder.fenwicktree import FenwickTree
+# from atcoder.fenwicktree import FenwickTree
 
 # available on Google, not available on Codeforces
 # import numpy as np
 # import scipy
 
 
-def solve(grid, n):  # fix inputs here
+def solve(arr, k):  # fix inputs here
     console("----- solving ------")
-    grid = [(x-1, y-1) for x,y in grid]
-    m = {}
-    for i,(x,y) in enumerate(grid):
-        m[x] = i
-    grid = sorted(grid)
-    console(grid)
+    console(arr,k)
 
-    fenwick_tree = FenwickTree(n+1)
-
-    res = [0 for _ in range(n)]
-
-    for i,(x,y) in enumerate(grid):
-        fenwick_tree.add(y, 1)
-        add = fenwick_tree.sum(0, y)
-        print(i,x,y,add)
-        res[x] += add
-
-    print()
-    fenwick_tree = FenwickTree(n+1)
-
-    for i,(x,y) in list(enumerate(grid))[::-1]:
-        add = fenwick_tree.sum(y, n)
-        fenwick_tree.add(y, 1)
-        print(i,x,y,add)
-        res[x] += add
-
-    console(res)
     
-    res2 = [0 for _ in range(n)]
-    for i,x in m.items():
-        res2[x] = res[i]
-    return res2
+
+    return 0
+
+
 
 
 def console(*args):  # the judge will not read these print statement
@@ -58,24 +33,24 @@ for case_num in [1]:
     # strr = input()
 
     # read line as an integer
-    nrows = int(inp[0])
+    # nrows = int(inp[0])
     
     # read one line and parse each word as a string
     # lst = input().split()
 
     # read one line and parse each word as an integer
-    # lst = list(map(int,input().split()))
+    nrows, k = list(map(int,inp[0].split()))
 
     # read matrix and parse as integers (after reading read nrows)
     # lst = list(map(int,input().split()))
     # nrows = lst[0]  # index containing information, please change
     grid = []
     for n in range(nrows):
-        grid.append(list(map(int,inp[n+1].split())))
+        grid.append(list(map(int,inp[n+1].split()))[0])
 
-    res = solve(grid, nrows)  # please change
+    res = solve(grid, k)  # please change
     
-    res = "\n".join(str(x) for x in res)
+    # res = "\n".join(str(x) for x in res)
     # Google - case number required
     # print("Case #{}: {}".format(case_num+1, res))
 
